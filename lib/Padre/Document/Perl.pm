b5ff42f321b3d97415564f4b2683f604ab805e85
Peter Lavender <peter.lavender@gmail.com>
2010-06-02 12:45:28 +0000

    Import of PLAVEN/Padre-0.63 from CPAN.
    gitpan-cpan-distribution: Padre
    gitpan-cpan-version:      0.63
    gitpan-cpan-path:         PLAVEN/Padre-0.63.tar.gz
    gitpan-cpan-author:       PLAVEN
    gitpan-cpan-maturity:     released

--- lib/Padre/Document/Perl.pm
+++ lib/Padre/Document/Perl.pm
@@ -17,7 +17,7 @@ use Padre::File                     ();
 use Padre::Document::Perl::Beginner ();
 use Padre::Logger;
 
-our $VERSION = '0.62';
+our $VERSION = '0.63';
 our @ISA     = 'Padre::Document';
 
 
@@ -1444,13 +1444,23 @@ sub event_on_char {
 			# which match the last key pressed (which is not part of
 			# $linetext at this moment:
 
-			if ( $linetext =~ /^sub[\s\t]+\w+$/ ) {
+			if ( $linetext =~ /^sub[\s\t]+(\w+)$/ ) {
+				my $subname = $1;
+
 				my $indent_string = $self->get_indentation_level_string(1);
 
 				# Add the default skeleton of a method
 				my $newline            = $self->newline;
 				my $text_before_cursor = " {$newline${indent_string}my \$self = shift;$newline$indent_string";
-				my $text_after_cursor  = "$newline}$newline";
+				$text_before_cursor =
+					  " {$newline${indent_string}my \$class = shift;$newline$newline"
+					. $indent_string
+					. "my \$self = bless {\@_}, \$class;$newline$newline"
+					. $indent_string
+					if $subname eq 'new';
+				my $text_after_cursor = "$newline}$newline";
+				$text_after_cursor = $newline . $indent_string . "return \$self;" . $text_after_cursor
+					if $subname eq 'new';
 				$editor->AddText( $text_before_cursor . $text_after_cursor );
 
 				# Ready for typing in the new method:
